2d7718ea20fd7745e8e2b060e19f8e572f8ebe05
0xR4UL <109295517+raulm0429@users.noreply.github.com>
2025-04-28 08:38:38 -0500

    Create open-meteo.py

---
+++ open-meteo.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import requests
+
+# Extract
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=37.69&longitude=-97.34&hourly=temperature_2m")
+data = response.json()
+
+print(data)
+
+# Transform
+df = pd.DataFrame({
+    'Time': data['hourly']['time'],
+    'Temperature (C)': data['hourly']['temperature_2m']
+})
+
+# Load
+df.to_csv('forecast.csv', index=False)
+print("✅ Weather forecast saved to CSV!")
